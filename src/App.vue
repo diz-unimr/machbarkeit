3fa3ae2ff530eca298626f5dedee09bc4256e45c
DESKTOP-389P100\Nattika <nattika.jugkaeo@uni-marburg.de>
2024-02-08 09:23:07 +0100

    fix stylelint error

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@
 				<div id="attribute-list-container">
 					<div id="attribute-list-header">Attributliste</div>
 					<div id="attribute-list-content">
-						<inpu
+						<input
 							id="search-input"
 							v-model="txtSearch"
 							type="text"
@@ -289,7 +289,7 @@ export default {
 	height: 15px;
 }
 
-input[type="text"] {
+input[type='text'] {
 	width: 100%;
 }
 
@@ -318,7 +318,6 @@ textarea {
 }
 
 #search-input {
-	padding-left: 5px;
 	background: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='15' height='15' viewBox='0 0 512 512' fill='gray'%3E%3Cpath d='M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z'%3E%3C/path%3E%3C/svg%3E");
 	background-repeat: no-repeat;
 	background-position-y: center;
