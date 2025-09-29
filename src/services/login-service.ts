ce3278d1f4f85dc1b553ebaf539ee94d0752a01a
Sebastian Stöcker <sebastian.stoecker@uni-marburg.de>
2025-09-29 14:00:08 +0200

    add reuse statement

--- src/services/login-service.ts
+++ src/services/login-service.ts
@@ -1,3 +1,8 @@
+/*
+	SPDX-FileCopyrightText: Nattika Jugkaeo <nattika.jugkaeo@uni-marburg.de>
+	SPDX-License-Identifier: AGPL-3.0-or-later
+*/
+
 export async function login() {
 
     const loginUrl = process.env.BACKEND_API_BASE + "/login?next=http://localhost:8080/index.php/apps/machbarkeit"
